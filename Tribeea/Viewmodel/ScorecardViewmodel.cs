02a3cf73616da16f6a21f5e7fe34713497f988b3
Tony Vatter <ajvatter@gmail.com>
2024-01-05 14:29:12 -0500

    Updates

---
+++ Tribeea/Viewmodel/ScorecardViewmodel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tribeea.Viewmodel
+{
+    public class ScorecardViewmodel
+    {
+        public string? Ranking { get; set; }
+        public string? TeamName { get; set; }
+        public int Score {  get; set; }
+    }
+}
